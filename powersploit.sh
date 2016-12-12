855a754fa50094fe8fd462b6af4b4ac1b30738f9
sproud22 <sproud22@gmail.com>
2016-12-12 12:21:24 -0500

    powersploit

---
+++ powersploit.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/mattifestation/PowerSploit.git /opt/PowerSploit
+cd /opt/PowerSploit && wget https://raw.githubusercontent.com/obscuresec/random/master/StartListener.py 
+wget https://raw.githubusercontent.com/darkoperator/powershell_scripts/master/ps_encoder.py
+exit
