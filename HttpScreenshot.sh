205c9cb0170750f6d77d493d6c99ddad8853baab
sproud22 <sproud22@gmail.com>
2016-12-12 11:07:38 -0500

    first upload

---
+++ HttpScreenshot.sh
@@ -0,0 +1,5 @@
+pip install selenium
+git clone https://github.com/breenmachine/httpscreenshot.git /opt/httpscreenshot
+cd /opt/httpscreenshot
+chmod +x install-dependencies.sh && ./install-dependencies.sh
+exit
