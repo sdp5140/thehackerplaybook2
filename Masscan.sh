cd66229649d24509383c521cddc4ed3b1e42b788
sproud22 <sproud22@gmail.com>
2016-12-12 11:20:58 -0500

    Masscan

--- Masscan.sh
+++ Masscan.sh
@@ -1,6 +1,5 @@
 apt-get install git gcc make libpcap-dev
-git clone-https://github.com/robertdavidgraham/masscangit /opt/masscan
+git clone https://github.com/robertdavidgraham/masscan.git /opt/masscan
 cd /opt/masscan
 make
-make install
 exit
