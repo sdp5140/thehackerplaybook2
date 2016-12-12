0bf16a7ee2f288f016e7e08612ec3b28e96851e1
sproud22 <sproud22@gmail.com>
2016-12-12 12:13:48 -0500

    spiderfoot.sh

---
+++ spiderfoot.sh
@@ -0,0 +1,10 @@
+mkdir /opt/spiderfoot/ 
+cd /opt/spiderfoot
+wget http://sourceforge.net/projects/spiderfoot/files/spiderfoot-2.3.0-src.tar.gz/download
+tar xzvf download
+pip install lxml
+pip install netaddr
+pip install M2Crypto
+pip install cherrypy
+pip install mako
+
