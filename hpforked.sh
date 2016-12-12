7f17f8364b040909a12dfc453f5e673a35f07544
sproud22 <sproud22@gmail.com>
2016-12-12 12:05:33 -0500

    hpforked

---
+++ hpforked.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/cheetz/PowerSploit /opt/HP_PowerSploit
+git clone https://github.com/cheetz/PowerTools /opt/HP_PowerTools
+git clone https://github.com/cheetz/nishang /opt/nishang
