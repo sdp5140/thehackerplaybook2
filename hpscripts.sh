e2c772e7f7bea997ed86498d75d195cd48d11844
sproud22 <sproud22@gmail.com>
2016-12-12 12:02:59 -0500

    hacker playbook tools

---
+++ hpscripts.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/cheetz/Easy-P.git /opt/Easy-P
+git clone https://github.com/cheetz/Password_Plus_One /opt/Password_Plus_One
+git clone https://github.com/cheetz/PowerShell_Popup /opt/PowerShell_Popup
+git clone https://github.com/cheetz/icmpshock /opt/icmpshock
+git clone https://github.com/cheetz/brutescrape /opt/brutescrape
+git clone https://www.github.com/cheetz/reddit_xss /opt/reddit_xss
